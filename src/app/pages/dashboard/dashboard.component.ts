9f92b293cad574fc7cb85133081e2454859a3393
Yan Koshelev <shrampro@gmail.com>
2018-11-01 22:58:21 +0300

    configure list

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { TodoListItem } from './../../shared/interfaces/index.interface';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { DashboardService } from './../../shared/services/dashboard.service';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { TitleAction } from 'src/app/shared/store/actions/app.action';
 import { Store } from '@ngrx/store';
 import { MainState } from 'src/app/shared/interfaces/index.interface';
 import { Unsubscribable } from 'src/app/utils/decorators/unsubscribable.decorator';
-import { FormControl, FormBuilder } from '@angular/forms';
+import { tap, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -28,20 +28,14 @@ export class DashboardComponent implements OnInit {
     this.store.dispatch(new TitleAction('Dashboard'));
 
     this.initVars();
-    this.initListeners();
   }
 
   public addTask(data: TodoListItem): void {
 
-    this.service.addTask(data)
-      .subscribe(console.log);
-  }
-
-  private initListeners(): void {
-    this.todoList$
-      .subscribe(data => {
-        console.log('data: ', data);
-      });
+    this.todoList$ =  this.service.addTask(data)
+      .pipe(
+        switchMap((list: TodoListItem[]) => of(list))
+      );
   }
 
   private initVars(): void {
--- src/app/shared/services/dashboard.service.ts
+++ src/app/shared/services/dashboard.service.ts
@@ -1,22 +1,17 @@
-import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TodoListItem } from '../interfaces/index.interface';
-import { mapToItem } from 'src/app/utils/functions/mappers.functions';
 
 @Injectable()
 export class DashboardService {
   constructor(private httpClient: HttpClient) { }
 
   public getTodosList(): Observable<TodoListItem[]> {
-    return this.httpClient.get(`/todos`)
-      .pipe(
-        map(mapToItem)
-      );
+    return this.httpClient.get<TodoListItem[]>(`/todos`);
   }
 
   public addTask(data: TodoListItem): Observable<any> {
-    return this.httpClient.post(`/add-task`, data);
+    return this.httpClient.put(`/add-task`, data);
   }
 }
